8a4fb8343de0d144f89a08c4f6f632e1b8c69681
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-03-19 19:25:41 -0400

    had to change import statement and corresponding methods, also extend throwable for exceptions

--- src/main/java/user_management/UserCollectionInitializer.java
+++ src/main/java/user_management/UserCollectionInitializer.java
@@ -1,24 +1,23 @@
-//package user_management;
-//
-//import org.apache.commons.io.IOUtils;
-//import parsing_json.ElementCollection;
-//
-//import java.io.IOException;
-//
-//public class UserCollectionInitializer {
-//
-//    public static UserCollection generate() {
-//        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-//        String json = null;
-//        try {
-//            json = IOUtils.toString(classLoader.getResourceAsStream("users.json"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//        return UserCollection.fromJSONString(json);
-//    }
-//}
-//
-//
+package user_management;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+
+public class UserCollectionInitializer {
+
+    public static UserCollection generate() {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        String json = null;
+        try {
+            json = IOUtils.toString(classLoader.getResourceAsStream("users.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return UserCollection.fromJSONString(json);
+    }
+}
+
+
