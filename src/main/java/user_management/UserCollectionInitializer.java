25ab5a37a1f70f6e8a0d10b23e1e7d4456e34fbd
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-03-19 17:11:39 -0400

    Element is complete except for where method, 23 of 24 tests passing

--- src/main/java/user_management/UserCollectionInitializer.java
+++ src/main/java/user_management/UserCollectionInitializer.java
@@ -1,7 +1,24 @@
-package user_management;
-
-public class UserCollectionInitializer {
-    public static UserCollection generate() {
-        return null;
-    }
-}
+//package user_management;
+//
+//import org.apache.commons.io.IOUtils;
+//import parsing_json.ElementCollection;
+//
+//import java.io.IOException;
+//
+//public class UserCollectionInitializer {
+//
+//    public static UserCollection generate() {
+//        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+//        String json = null;
+//        try {
+//            json = IOUtils.toString(classLoader.getResourceAsStream("users.json"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        return UserCollection.fromJSONString(json);
+//    }
+//}
+//
+//
